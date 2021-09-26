cb2c9ada66abfc677a8573ae933997e9188c4ece
HuaHero <huahero.long@gmail.com>
2021-09-26 11:31:13 +0800

    Create Person.java

---
+++ DynamicDroolsFile/src/main/java/com.huawei.hqh.model/Person.java
@@ -0,0 +1,28 @@
+package com.huawei.hqh.model;
+
+/**
+ * 功能描述
+ *
+ * @author h00284904
+ * @since 2021-09-24
+ */
+public class Person {
+    private String name;
+    private int age;
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public int getAge() {
+        return age;
+    }
+}
